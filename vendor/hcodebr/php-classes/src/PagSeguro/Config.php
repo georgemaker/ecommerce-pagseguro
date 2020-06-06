901f4d4d8a2b2223a3cc048ec2d16078961cfdfd
George <suportesgmaker@gmail.com>
2020-06-05 22:48:00 -0300

    Order na Session e Payment Layout

--- vendor/hcodebr/php-classes/src/PagSeguro/Config.php
+++ vendor/hcodebr/php-classes/src/PagSeguro/Config.php
@@ -12,9 +12,11 @@ class Config {
     const SANDBOX_TOKEN = "5BEE5651DDA344089236CD185379B11C";
     const PRODUCTION_TOKEN = "994c14a2-20d1-4648-8ea7-7d6d38354d1a8f696fc747c99842137380e9084a688431cc-ee07-47cf-9cc2-3800343152ca";
 
-    const SANDBOX_SESSIONS = "https://sandbox.api.pagseguro.com/charges";
-    const PRODUCTION_SESSIONS = "https://api.r2tec.com/edi/v1/";
-    //const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/v2/sessions";
+    const SANDBOX_SESSIONS = "https://ws.sandbox.pagseguro.uol.com.br/?_ga=2.165561023.1460041926.1591287068-1809990948.1591287065";
+    const PRODUCTION_SESSIONS = "https://ws.pagseguro.uol.com.br/?_ga=2.165440063.1460041926.1591287068-1809990948.1591287065";
+    
+    const SANDBOX_URL_JS = "https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
+    const PRODUCTION_URL_JS = "https://stc.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js";
 
     public static function getAuthentication():array
     {
@@ -42,4 +44,12 @@ class Config {
         return (Config::SANDBOX === true) ? Config::SANDBOX_SESSIONS : Config::PRODUCTION_SESSIONS;
 
     }
+
+    public static function getUrlJS()
+    {
+
+        return (Config::SANDBOX === true) ? Config::SANDBOX_URL_JS : Config::PRODUCTION_URL_JS;
+
+    }
+    
 }
